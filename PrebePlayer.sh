50f4a1c1e8daeaff125456dcbb47b00a59271eb6
David Silva <david-lop97@hotmail.com>
2018-03-07 15:08:48 -0600

    Actualizacion de la prebeplayer. Ya acepta espacios.

--- PrebePlayer.sh
+++ PrebePlayer.sh
@@ -19,14 +19,14 @@ echo -e '\n\e[32m- Biblioteca Musical -\n \e[94m'
 more Musica/Lista.txt
 echo -e '------------------------------------------------------------------------------------------------'
 
-echo '(Introduce el nombre de la cancion sin espacios)'		#En posibilidad de cambios
+echo '(Introduce el nombre de la cancion)'		#Ya acepta espacios
 echo 'Reproducir: '
 read name
 echo -e 'Reproduciendo...\n'
 echo 'Pausar/reproducir [s] o barra espaciadora [ ]' #banderas propias del comando mpg123
 echo 'Reproducir otra cancion presione [q]'
 echo -ne "\033]0;Estás escuchando: $name\007"
-mpg123 -q Musica/$name.mp3		#Para esto es necesario tener mpg123 instalado (investigar bien sus banderas)
+mpg123 -q "Musica/$name.mp3"		#Para esto es necesario tener mpg123 instalado (investigar bien sus banderas)
 echo -ne "\033]0;Terminal\007"
 }
 
